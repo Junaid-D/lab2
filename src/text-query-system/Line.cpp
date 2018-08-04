c8b8f0207c1034ac6179dcb8af1e7271c220687f
Junaid Dawood <1094837@students.wits.ac.za>
2018-08-04 09:32:18 +0200

    Exercise 5.2

--- src/text-query-system/Line.cpp
+++ src/text-query-system/Line.cpp
@@ -3,14 +3,47 @@
 // -------------------------------------------
 
 #include "Line.h"
-
+#include <algorithm>
+#include <iostream>
 Line::Line(const string& line)
 {
 	// Hint: some of string's member functions might come in handy here
 	// for extracting words.
+    size_t delimPos,startPos=0;
+   
+  
+    auto delimiter=' ';
+    if(line.find(delimiter)==string::npos )
+    {
+        try{
+            lineWords.push_back(Word(line));
+        }
+        catch(WordContainsNoLetters&)
+        {
+            cerr<<"Trying to create an empty word"<<endl;
+        }
+        return;
+    }
+    while((delimPos=line.find(delimiter,startPos))!=string::npos)
+    {
+        try{
+            lineWords.push_back(Word(line.substr(startPos,delimPos-startPos)) );
+        }
+        catch(WordContainsNoLetters&)
+        {
+            cerr<<"Trying to create an empty word"<<endl;
+        }
+        startPos=delimPos+1;
+        
+    }
+    if(startPos<line.size()){
+        lineWords.push_back(Word(line.substr(startPos,line.size()-startPos)));
+        
+    }
 }
 
 bool Line::contains(const Word& search_word) const
 {
-	return false;
+  
+	return (std::find(lineWords.begin(),lineWords.end(),search_word)!=lineWords.end()&&search_word.isQueryable());
 }
